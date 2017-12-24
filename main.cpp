dbcec315328ff9b60e66735371172dddfe351484
Darkmyter <bader.d24@gmail.com>
2017-12-24 12:17:49 +0100

    Working on read data

--- main.cpp
+++ main.cpp
@@ -18,29 +18,61 @@ int main()
     return 0;
 }
 
-Matrix read_data(){
+Rating * fill_rating(){ //creat set of all ratings
     
     ifstream in_stream;
     int loop=1;
     string line;
-    float a, b, c;
+    float rate,movieid,userid;
+    Rating *R = new Rating[855599]
 
-    //Check input and output file.
-    in_stream.open("test.txt");
+    //Open input file.
+    in_stream.open("/data/user_ratedmovies-timestamps.dat");
 
     if (in_stream.fail())
     {
         cout << "Input file opening failed";
         exit(1);
     }
+
     while (! in_stream.eof() ) //Runs while the file is NOT at the end
 		{
-            getline(in_stream,line);//Gets a single line from example.txt
-			std::istringstream iss(line);
-            iss >>a>>b>>c;
-            cout<<a<<" "<<b<<" "<<c<<endl; //Saves that line in the array
+            getline(in_stream,line);//Gets a single line from file
+			std::istringstream iss(line); //get numbers in the line
+            iss >>user>>movie>>rate;
+            R[loop] = Rating(rate,user,movie);
 			loop++; //Does an increment to the variable 'loop'
 		}
 		in_stream.close(); //Closes the file
+    return R;
+}
 
+Rating * fill_rating(){ //creat set of all movies
+    
+    ifstream in_stream;
+    int loop=1;
+    string line;
+    float id;
+    string title;
+    Movie *M = new Movie[10198]
+
+    //Open input file.
+    in_stream.open("/data/movies.dat");
+
+    if (in_stream.fail())
+    {
+        cout << "Input file opening failed";
+        exit(1);
+    }
+
+    while (! in_stream.eof() ) //Runs while the file is NOT at the end
+		{
+            getline(in_stream,line);//Gets a single line from file
+			std::istringstream iss(line); //get numbers in the line
+            iss >>id>>title;
+            M[loop] = Movie(id,title);
+			loop++; //Does an increment to the variable 'loop'
+		}
+		in_stream.close(); //Closes the file
+    return M;
 }
